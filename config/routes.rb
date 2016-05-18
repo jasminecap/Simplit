9ea79530f307db8b07e566429555eaf14978c126
Jasmine <capote.jasmine@icloud.com>
2016-05-18 18:27:21 -0400

    login tested and works so far

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
 
-  get 'signup', to: 'user#new', as: 'signup'
+  get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  root 'sessions#signup'
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
