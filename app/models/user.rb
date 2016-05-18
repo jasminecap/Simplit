97a50558b45469724dbe7101cfe0c3c3c2a7a9f1
Jasmine <capote.jasmine@icloud.com>
2016-05-18 18:10:44 -0400

    user validations yaas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :username, presence: true, uniqueness: true
+  validates :email, uniqueness: true, format: {
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+  }
+
+  def to_s
+    "#{username}"
+  end
 end
