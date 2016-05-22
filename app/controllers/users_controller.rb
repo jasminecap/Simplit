66b38e48c21b6133f0e7468342814babc7fc4c64
Jasmine <capote.jasmine@icloud.com>
2016-05-22 16:05:35 -0400

    its prettier now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,11 +25,11 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-    
+
     respond_to do |format|
       if @user.save
         session[:user_id] = @user.id
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to root_url, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
