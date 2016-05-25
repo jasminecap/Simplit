1200343bda026264e2375d1a4e31cf6719249f2c
Deanna Melin <deannamelin@gmail.com>
2016-05-25 17:06:25 -0400

    changed db from sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160522162348) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
