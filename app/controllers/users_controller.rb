103acef22589272d2da4af15faf27cc14517dc1e
Jasmine <capote.jasmine@icloud.com>
2016-05-25 18:02:38 -0400

    signup FIXED

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,6 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
     respond_to do |format|
       if @user.save
         session[:user_id] = @user.id
