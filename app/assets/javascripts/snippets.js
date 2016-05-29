fe977ada7e6910dcd3ca7436027d9d332c8bf00f
Deanna Melin <deannamelin@gmail.com>
2016-05-29 17:08:05 -0400

    changed snippets page to have collapsible form

--- app/assets/javascripts/snippets.js
+++ app/assets/javascripts/snippets.js
@@ -1,12 +1,3 @@
-// (function($){
-//   $(function(){
-//
-//     $('.button-collapse').sideNav();
-//     $('.parallax').parallax();
-
-  }); // end of document ready
-// })(jQuery); // end of jQuery name space
-
 $(document).ready(function(){
    $('.collapsible').collapsible({
      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
