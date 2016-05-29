9a63fab99cfb1643f19bc55cfffeb8e737f839be
Jasmine <capote.jasmine@icloud.com>
2016-05-29 16:22:58 -0400

    snippets pages styling and linking fixed

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -28,7 +28,7 @@ class SnippetsController < ApplicationController
 
     respond_to do |format|
       if @snippet.save
-        format.html { redirect_to @snippet, notice: 'Snippet was successfully created.' }
+        format.html { redirect_to snippets_url}
         format.json { render :show, status: :created, location: @snippet }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class SnippetsController < ApplicationController
   def update
     respond_to do |format|
       if @snippet.update(snippet_params)
-        format.html { redirect_to @snippet, notice: 'Snippet was successfully updated.' }
+        format.html { redirect_to snippets_url}
         format.json { render :show, status: :ok, location: @snippet }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class SnippetsController < ApplicationController
   def destroy
     @snippet.destroy
     respond_to do |format|
-      format.html { redirect_to snippets_url, notice: 'Snippet was successfully destroyed.' }
+      format.html { redirect_to snippets_url}
       format.json { head :no_content }
     end
   end
